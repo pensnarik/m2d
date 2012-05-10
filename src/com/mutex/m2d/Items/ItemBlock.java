4fbc193ae9aec31d9b9c78175177b2057a5b79d3
Andrey Zhidenkov <agwts@mail.ru>
2012-05-10 20:32:45 +0400

    ItemBlock added.

---
+++ src/com/mutex/m2d/Items/ItemBlock.java
@@ -0,0 +1,13 @@
+package com.mutex.m2d.Items;
+
+import com.mutex.m2d.*;
+
+public class ItemBlock extends Item
+{
+	public int blockID;
+	
+	public ItemBlock(int itemID_)
+	{
+		super(itemID_);
+	}
+}
